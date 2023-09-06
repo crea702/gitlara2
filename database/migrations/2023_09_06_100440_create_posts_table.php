ebf78c452673965372163a5b4ae7f5c3f85c0197
Crea702 <sir.02.aib@gmail.com>
2023-09-06 16:26:28 +0500

    создал миграцию

--- database/migrations/2023_09_06_100440_create_posts_table.php
+++ database/migrations/2023_09_06_100440_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new  class extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->String('title');
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('likes')->nullable();
+            $table->boolean('is_published')->default(1);
             $table->timestamps();
         });
     }
