be217ce901f0bb80590e0fdf4506825bc8572fdb
Crea702 <sir.02.aib@gmail.com>
2023-09-12 09:57:07 +0500

    создал view part 2

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,14 @@ Route::get('/', function () {
     return "sjuk";
 });
 
-Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index']);
+Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [\App\Http\Controllers\PostController::class, 'create']);
 Route::get('/posts/update', [\App\Http\Controllers\PostController::class, 'update']);
 Route::get('/posts/delete', [\App\Http\Controllers\PostController::class, 'delete']);
 Route::get('/posts/restore', [\App\Http\Controllers\PostController::class, 'restore']);
 Route::get('/posts/first_or_create', [\App\Http\Controllers\PostController::class, 'firstOrCreate']);
 Route::get('/posts/update_or_create', [\App\Http\Controllers\PostController::class, 'updateOrCreate']);
+
+Route::get('/main', [\App\Http\Controllers\MainController::class, 'index'])->name('main.index');
+Route::get('/contacts', [\App\Http\Controllers\ContatsController::class, 'index'])->name('contact.index');
+Route::get('/about', [\App\Http\Controllers\AboutController::class, 'index'])->name('about.index');
